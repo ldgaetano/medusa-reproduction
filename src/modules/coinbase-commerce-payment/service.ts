92d34742efffddda54b1a2cc4e2acdad60070fa7
Luca D'Angelo <ldgaetano@protonmail.com>
2024-12-02 15:34:33 +0900

    Setup coinbase commerce payment provider.

---
+++ src/modules/coinbase-commerce-payment/service.ts
@@ -0,0 +1,11 @@
+import { AbstractPaymentProvider } from "@medusajs/framework/utils"
+
+type Options = {
+    apiKey: string
+}
+
+class CoinbaseCommercePaymentProviderService extends AbstractPaymentProvider<Options> {
+    // TODO: Implement payment provider methods.
+}
+
+export default CoinbaseCommercePaymentProviderService
